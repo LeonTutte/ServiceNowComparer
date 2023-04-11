12f7a8ba6334c0badc06ff366a4cc62cb930112f
Leon Tutte <leonacc@ltutte.de>
2023-04-11 14:22:10 +0200

    Implemented validationerrors in Logmodule

--- ServiceNowComparerLibrary/Modules/Static/LogModule.cs
+++ ServiceNowComparerLibrary/Modules/Static/LogModule.cs
@@ -1,3 +1,5 @@
+using FluentValidation.Results;
+
 using Serilog;
 using Serilog.Core;
 
@@ -32,5 +34,21 @@ namespace ServiceNowComparerLibrary.Modules.Static
         /// <param name="message">Your Message for the Log.</param>
         /// <returns></returns>
         public static void WriteDebug(string message) => _logger.Debug(message);
+
+        public static void ProcessValidationErrors(ValidationResult result)
+        {
+            if (result == null)
+            {
+                WriteError("Validationresults were empty");
+                return;
+            }
+            for (int i = 0; i < result.Errors.Count; i++)
+            {
+                ValidationFailure failure = result.Errors[i];
+                WriteError($"{failure.PropertyName}: {failure.ErrorMessage}");
+            }
+            return;
+
+        }
     }
 }
